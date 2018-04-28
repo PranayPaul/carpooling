79db89103178cc8f0146ac66650585ca8b4a258c
PranayPaul <30439699+PranayPaul@users.noreply.github.com>
2018-04-28 20:14:46 +0530

    Update junk.java

--- junk.java
+++ junk.java
@@ -1,23 +1,28 @@
-	public static double RangePickup( double startx , double endx , double starty , double endy , Driver d , Passenger p   ){
+	public static double RangePickup( double startx , double endx , double starty , double endy , Driver d , Passenger p ){
 		    
 		    double globalx = endx - startx;
 		    double globaly = endy - starty;
 		    double localx = p.x - d.x;
 		    double localy = p.y - d.y;
-		    if( globalx == 0 ) {
-		    	globalx == 1;
+		    double currentx = endx - d.x;
+		    double currenty = endy - d.y;
+		    double value;
+		    if( globalx * localx <= 0 ) {
+		    	valuex = math.abs(localx);
 		    }
+		    else
+			valuex = 0;
+		    if( globaly * localy <= 0 ){
+			    valuey = math.abs(localx);
+		    }
+		    else
+	               value = 0;
+		   double backtrack = valuex + valuey;
 		    if( globaly == 0 ) {
-		    	globaly == 1;
+		    	valuey == 1;
 		    }
-		    double divx = localx / globalx;
-		    double divy = localy / globaly;
-		    if( divx > 0 ) {
-		    	divx = 0;
-		    if( divy > 0 )
-		    	divy = 0;
-		    double track = -( divx + divy );
-		    return track;
+		    double range = 1.25 * (1 - manhatten( currentx , currenty ) / manhatten( globalx , globaly ));
+		    return backtrack;
 		    }
 		    }
 		    
