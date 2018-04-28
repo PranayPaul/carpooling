1720d95f78785247e11e658c5e47ede7b8a08eff
PranayPaul <30439699+PranayPaul@users.noreply.github.com>
2018-04-28 14:17:58 +0530

    Update junk.java

--- junk.java
+++ junk.java
@@ -1,4 +1,4 @@
-	public static int rangepickup ( int x1 , int x2 , int y1 , int y2 , int x3 , int x4 , int y3 , int y4 ){
+	public static int RangePickup ( int x1 , int x2 , int y1 , int y2 , int x3 , int x4 , int y3 , int y4 ){
 		    //x1,y1 origin x2,y2 destination of the route
 		    //x3 y3 driver urrent position x4 y4 passeger current position
 	    	int v1 = manhattan ( x1 , x2 , y1 , y2 );
